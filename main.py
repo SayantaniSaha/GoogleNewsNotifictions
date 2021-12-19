b23d7ff4a07a1f46ab3212a7e41ddb5140f9bc74
SayantaniSaha <sayantani.s1023@gmail.com>
2021-12-19 20:19:01 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: Sayantani
+
+"""
+import time
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from plyer import notification
+
+
+url = "https://news.google.com/news/rss"
+xml_data = urlopen(url).read()
+urlopen(url).close()
+    
+sp = BeautifulSoup(xml_data, "xml")
+news_list = sp.find_all('item')
+news_list = news_list[0:10]
+
+title = "Top News Headline of the Hour"
+
+
+while True:
+    for news in news_list:
+        message = news.title.text
+        notification.notify(title=title, message=message, timeout=20)
+    time.sleep(3600)
